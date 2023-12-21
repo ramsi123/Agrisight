78b17c62c5f18bb77835d4256ea00d30cc7b9e2b
Dhiya' Ulhaq Ahmad Ramsi <67065679+ramsi123@users.noreply.github.com>
2023-12-21 13:24:00 +0700

    Delete app directory

--- app/src/main/java/com/example/capstoneproject/data/domain/Classification.kt
+++
@@ -1,6 +0,0 @@
-package com.example.capstoneproject.data.domain
-
-data class Classification(
-    val name: String,
-    val score: Float
-)
--- app/src/main/java/com/example/capstoneproject/data/domain/LandmarkClassifier.kt
+++
@@ -1,7 +0,0 @@
-package com.example.capstoneproject.data.domain
-
-import android.graphics.Bitmap
-
-interface LandmarkClassifier {
-    fun classify(bitmap: Bitmap, rotation: Int): List<Classification>
-}
